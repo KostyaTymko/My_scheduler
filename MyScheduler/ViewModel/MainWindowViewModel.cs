881648a24683e89ec750477b9797fbff2cd6535c
KostyaTymko <kostyatymko@gmail.com>
2017-08-31 09:47:14 +0300

    first in dev

--- MyScheduler/ViewModel/MainWindowViewModel.cs
+++ MyScheduler/ViewModel/MainWindowViewModel.cs
@@ -14,6 +14,7 @@ namespace MyScheduler.ViewModel
 {
     class MainViewModel
     {
+        private int flag;
         private MyTask selectedTask;
         public ObservableCollection<MyTask> Tasks { get; set; }
 
@@ -28,7 +29,12 @@ namespace MyScheduler.ViewModel
         }
         public MainViewModel()
         {
-            ClickCommand = new Command(arg => ClickMethod());
+            flag = 0;
+            ClickButtonCommand = new Command(arg => ClickButtonMethod());
+            ClickSearchCommand = new Command(arg => ClickSearchMethod());
+            ClickAddCommand = new Command(arg => ClickAddMethod());
+            ClickEditCommand = new Command(arg => ClickEditMethod());
+            ClickDeleteCommand = new Command(arg => ClickDeleteMethod());
             Shedules_Singleton s = Shedules_Singleton.getInstance("DbTasks");
             Tasks = s.Tasks;
         }
@@ -47,17 +53,47 @@ namespace MyScheduler.ViewModel
         /// <summary>
         /// Get or set ClickCommand.
         /// </summary>
-        public ICommand ClickCommand { get; set; }
+        public ICommand ClickButtonCommand { get; set; }
+        public ICommand ClickSearchCommand { get; set; }
+        public ICommand ClickAddCommand { get; set; }
+        public ICommand ClickEditCommand { get; set; }
+        public ICommand ClickDeleteCommand { get; set; }
         #endregion
 
         #region Methods
         /// <summary>
         /// Click method.
         /// </summary>
-        private void ClickMethod()
+        private void ClickButtonMethod()
         {
-            MessageBox.Show("This is click command.");
+
+            MessageBox.Show("button");
+        }
+        private void ClickSearchMethod()
+        {
+            MessageBox.Show("search");
+            //button.Visibility = Visibility.Visible;
+            //NAME = "Changed Name";
+            //button1.Content = ;
+        }
+        private void ClickAddMethod()
+        {
+            MessageBox.Show("add");
+        }
+        private void ClickEditMethod()
+        {
+            MessageBox.Show("edit");
+        }
+        private void ClickDeleteMethod()
+        {
+            MessageBox.Show("add");
         }
         #endregion
+
+
+
+
+
+
     }
 }
